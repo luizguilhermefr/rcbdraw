f9aa8ed0c6a5ecf3caea6c281c4808b9dc840d90
Luiz <luizguilhermefr@gmail.com>
2017-05-17 13:39:20 -0300

    painel principal (not working)

---
+++ js/Interface.js
@@ -0,0 +1,15 @@
+function Interface (newCanvas) {
+  this.context = newCanvas.getContext('2d');
+  //this.context.beginPath();
+
+  /*this.drawExample = function () {
+    this.context.moveTo(75, 250);
+    this.context.lineTo(150, 50);
+    this.context.lineTo(225, 250);
+    this.context.lineTo(50, 120);
+    this.context.lineTo(250, 120);
+    this.context.lineTo(75, 250);
+    this.context.lineWidth = 2;
+    this.context.strokeStyle = 'red';
+  };*/
+}
