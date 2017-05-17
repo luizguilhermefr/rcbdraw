f274a41d567bcfbff9d13f99867ceb845000c032
Luiz <luizguilhermefr@gmail.com>
2017-05-17 14:15:19 -0300

    styleguide

--- js/Interface.js
+++ js/Interface.js
@@ -1,15 +1,21 @@
 function Interface (newCanvas) {
-  this.context = newCanvas.getContext('2d');
-  //this.context.beginPath();
+    this.canvas = newCanvas;
+    this.context = this.canvas.getContext('2d');
+    this.context.beginPath();
 
-  /*this.drawExample = function () {
-    this.context.moveTo(75, 250);
-    this.context.lineTo(150, 50);
-    this.context.lineTo(225, 250);
-    this.context.lineTo(50, 120);
-    this.context.lineTo(250, 120);
-    this.context.lineTo(75, 250);
-    this.context.lineWidth = 2;
-    this.context.strokeStyle = 'red';
-  };*/
+    // this.drawExample = function () {
+    //   this.context.lineWidth = 2;
+    //   this.context.strokeStyle = 'red';
+    //   this.context.moveTo(75, 250);
+    //   this.context.lineTo(150, 50);
+    //   this.context.lineTo(225, 250);
+    //   this.context.lineTo(50, 120);
+    //   this.context.lineTo(250, 120);
+    //   this.context.lineTo(75, 250);
+    //   this.context.stroke();
+    // };
+
+    this.clearAll = function () {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    };
 }
