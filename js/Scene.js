de03e5f596d05d6370c46470757ee6c1271426c7
luizguilhermefr <luizguilhermefr@gmail.com>
2017-07-07 15:43:56 -0300

    ask on new

--- js/Scene.js
+++ js/Scene.js
@@ -1,14 +1,18 @@
 function Scene () {
     this.polygons = [];
 
+    this.dirty = false;
+
     this.addPolygon = function (polygon) {
         this.polygons.push(polygon);
+        this.dirty = true;
     };
 
     this.removePolygon = function (id) {
         for (var i = 0; i < this.polygons.length; i++) {
-            if ( i === id) { 
+            if (i === id) {
                 this.polygons.splice(id, 1);
+                this.dirty = true;
                 return true;
             }
         }
@@ -17,5 +21,13 @@ function Scene () {
 
     this.getPolygons = function () {
         return this.polygons;
+    };
+
+    this.isDirty = function () {
+        return this.dirty;
+    };
+
+    this.resetDirt = function () {
+        this.dirty = false;
     };
 }
