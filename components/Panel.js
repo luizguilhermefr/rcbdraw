00416ffbc6d8c3c32d20daf265a64d8bec0a9e1a
luizguilhermefr <luizguilhermefr@gmail.com>
2017-08-28 15:09:11 -0300

    contex menu

--- components/Panel.js
+++ components/Panel.js
@@ -109,7 +109,6 @@ Vue.component('panel', {
         },
         putPoly (x, y) {
             drawInterface.newRegularPolygon(this.sides, this.size, this.stroke, this.fill, this.mustStroke, this.mustFill, x, y);
-            this.reset();
         },
         selectionClick (x, y) {
             drawInterface.selectionClick(x, y);
